e1f08a9445c6f197e70e7dbb65483a9498a3e6c8
mironov-maksim <mironov.maksim.1994@gmail.com>
2022-03-24 19:43:09 +0200

    added some changes

--- growbe/js/script.js
+++ growbe/js/script.js
@@ -1,26 +1,64 @@
 const slider_1 = new Swiper(".testimonials__slider", {
-  slidesPerView: 3,
+  /* slidesPerView: 3, */
   spaceBetween: 30,
   loop: true,
-  slidesOffsetBefore: 320,
-  slidesOffsetAfter: 320,
+  /* slidesOffsetBefore: 320,
+  slidesOffsetAfter: 320, */
   centeredSlides: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    1900: {
+      slidesPerView: 3,
+      slidesOffsetBefore: 330,
+      slidesOffsetAfter: 330,
+    },
+
+    1800: {
+      slidesPerView: 3,
+      slidesOffsetBefore: 315,
+      slidesOffsetAfter: 315,
+    },
+
+    1700: {
+      slidesPerView: 3,
+      slidesOffsetBefore: 300,
+      slidesOffsetAfter: 300,
+    },
+
+    1000: {
+      slidesPerView: 2,
+      slidesOffsetBefore: 0,
+      slidesOffsetAfter: 0,
+    },
+
+    420: {
+      slidesPerView: 1,
+      slidesOffsetBefore: 0,
+      slidesOffsetAfter: 0,
+    },
+  },
 });
 
 const slider_2 = new Swiper(".news__slider", {
   loop: true,
-  slidesPerView: 2,
   spaceBetween: 30,
-  /*  slidesOffsetBefore: 10,
-  slidesOffsetAfter: 10, */
   centeredSlides: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+
+  breakpoints: {
+    1400: {
+      slidesPerView: 2,
+    },
+
+    420: {
+      slidesPerView: 1,
+    },
   },
 });
 
@@ -28,14 +66,26 @@ new WOW().init();
 
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".menu");
-const menuLink = document.querySelectorAll(".menu__link");
+const menuLink = document.querySelectorAll(".menu__link--js");
 const menuOverlay = document.querySelector(".menu-overlay");
+const offMenuLink = document.querySelector(".menu__link--offdefault");
+
+offMenuLink.addEventListener("click", function (e) {
+  e.preventDefault();
+});
 
 burger.addEventListener("click", function () {
   burger.classList.toggle("burger--active");
   menu.classList.toggle("menu--active");
   document.body.classList.toggle("no-scroll");
   menuOverlay.classList.toggle("menu-overlay--active");
+});
+
+menuOverlay.addEventListener("click", function () {
+  burger.classList.remove("burger--active");
+  menu.classList.remove("menu--active");
+  menuOverlay.classList.remove("menu-overlay--active");
+  document.body.classList.remove("no-scroll");
 });
 
 menuLink.forEach((item) => {
